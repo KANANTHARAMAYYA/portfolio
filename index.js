656a91c055f55888a1d76628668fd35c13b2df73
KANANTHARAMAYYA <kanantharamayya@gmail.com>
2022-01-02 09:52:50 -0700

    Updates

--- index.js
+++ index.js
@@ -28,5 +28,8 @@ camera.position.z = 5;
 function render() {
   requestAnimationFrame( render );
   renderer.render( scene, camera );
+  renderer.render(scene, camera);
+  cube.rotation.y += 0.01;
+  cube.rotation.x += 0.01;
 }
 render();
